387de39a94b255bddd2a87cfe383afcaec59ae88
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-01-05 20:24:30 +0530

    initial

---
+++ src/components/Body/Body.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {ArrowDown} from 'react-feather'
+
+import styles from "./Body.module.css";
+
+function Body() {
+  const colors = ["239ce2","#48bb78","#0bc5ea","#a0aec0","#ed8936"];
+  return (
+    <div className={styles.container}>
+    <p className={styles.heading}>Resume Builder</p>
+    <div className={styles.toolbar}>
+      <div className={styles.colors}>
+        {colors.map((item) => (
+          <span
+          key={item}
+          style={{ backgroundColor: item}}
+          className={styles.color}
+          />
+        ))}
+      </div>
+      <button>Download</button>
+    </div>
+    </div>
+  )
+}
+
+export default Body
