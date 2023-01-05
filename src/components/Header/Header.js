b1609bf9bbb2377334b18ba29015d11231ef79b4
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-01-06 01:47:05 +0530

    Project Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import resumeSvg from '../../assets/undraw_resume.svg';
-import styles from './Header.module.css'
+import React from "react";
+
+import resumeSvg from "../../assets/undraw_resume.svg";
+
+import styles from "./Header.module.css";
 
 function Header() {
-    return (
+  return (
     <div className={styles.container}>
-        <div className={styles.left}>
-        <p className={styles.heading}>A <span>Resume</span>     that stands out!</p>
-        <p className={styles.heading}>Make your own resume. <span>It's free</span></p>
-        </div> 
-        <div className={styles.right}>
-        <img src={resumeSvg} alt=" Resume "/>    
-        </div>  
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume</span> that stands out!
+        </p>
+        <p className={styles.heading}>
+          Make your own resume. <span>It's free</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resumeSvg} alt="Resume" />
+      </div>
     </div>
   );
 }
