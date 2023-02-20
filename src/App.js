8a669f5579f058b0188090081960234194325b79
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-02-21 03:34:19 +0530

    Login setup1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header/Header";
-// import Body from "./components/Body/Body";
 import Login from "./components/Login/Login"
 import Signup from "./components/Signup/Signup"
 import "./App.css";
