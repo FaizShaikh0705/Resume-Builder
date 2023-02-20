92746831a3428678efe30fd85ea3589303228522
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-02-21 03:38:21 +0530

    Login setup11

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  
+   <div>
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="*" element={<Header />} />
         </Routes>
-      </Router>
-
+      </Router> 
+       </div>
+    
 
   );
 }
